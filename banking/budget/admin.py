a3c4a5af7c10bd2b2708da02f0300ca8d2c37489
andrew <drewblay@gmail.com>
2016-11-07 21:58:59 -0500

    cleanup

--- banking/budget/admin.py
+++ banking/budget/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Budget, MonthlyBudget, SavingsGoal
+from .models import Budget, MonthlyBudget
 
 admin.site.register(Budget)
 admin.site.register(MonthlyBudget)
-admin.site.register(SavingsGoal)
